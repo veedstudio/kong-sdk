0db012f5bbf00cc60107ffcdd87a5900f024ee2b
Jacobus Meulen <sjaakiejj@gmail.com>
2021-06-01 09:05:30 +0700

    Move index file into root

---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from './src/client';
+export * from './src/models';
+export * from './src/errors/APIError';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './client';
-export * from './models';
-export * from './errors/APIError';
