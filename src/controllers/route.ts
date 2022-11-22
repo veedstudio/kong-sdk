0d96ba1720a7ac075d10b55362397be3148489a5
Jacobus Meulen <sjaakiejj@gmail.com>
2022-11-23 00:05:00 +0700

    Add getRoutesForService call

--- src/controllers/route.ts
+++ src/controllers/route.ts
@@ -5,4 +5,8 @@ export class RouteController extends BaseController<Route> {
   getResourcePath(): string {
     return 'routes';
   }
+
+  async getRoutesForService(serviceIdOrName: string): Promise<Route[]> {
+    return this.performRequest('get', `services/${serviceIdOrName}/routes`);
+  }
 }
